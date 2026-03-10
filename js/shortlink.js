ce8794e4e6ef0dcdb7d704fb0fefc1d337d245ae
giangndptit <giangnd.ptit@gmail.com>
2026-03-10 18:03:23 +0700

    Create shortlink.js

---
+++ js/shortlink.js
@@ -0,0 +1,10 @@
+const redirectToLink = (url) => {
+    window.location.href = url;
+}
+
+const main = () => {
+    redirectToLink('attendance://link');
+    redirectToLink('bizappstore://link');
+}
+
+main();
